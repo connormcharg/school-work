001848d7f91c2af2a5ec04c8f94b50aa83d69f27
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-10-08 23:10:02 +0100

    reverted back as i am gonna write it myself properly

--- NEA-Final/RooksRealm/frontend/src/components/ChessBoard.tsx
+++ NEA-Final/RooksRealm/frontend/src/components/ChessBoard.tsx
@@ -1,19 +1,6 @@
-import React, { useState, useRef, useEffect } from "react";
-import "./ChessBoard.css";
+import React from "react";
 
-interface ChessBoardProps {
-    isWhite: boolean;
-    board: string[][];
-    onMoveValidCheck: (start: number[], end: number[]) => Promise<boolean>;
-    onMakeMove: (start: number[], end: number[]) => Promise<void>;
-}
-
-const ChessBoard: React.FC<ChessBoardProps> = ({ isWhite, board, onMoveValidCheck, onMakeMove }) => {
-    const [selectedPiece, setSelectedPiece] = useState<HTMLElement | null>(null);
-    const [selectedPieceInitialRowCol, setSelectedPieceInitialRowCol] = useState<number[] | null>(null);
-    const [state, setState] = useState<number>(1);
-    const gridRef = useRef<HTMLDivElement>(null);
-
+const ChessBoard: React.FC = () => {
     return (
         <div>
 
--- NEA-Final/RooksRealm/frontend/src/components/PlayGame.tsx
+++ NEA-Final/RooksRealm/frontend/src/components/PlayGame.tsx
@@ -1,37 +1,13 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import ChessBoard from "./ChessBoard";
 
-const PlayGame: React.FC = () => {
-    const onMoveValidCheck = async (start: number[], end: number[]): Promise<boolean> => {
-        return true;
-    };
-    const onMakeMove = async (start: number[], end: number[]): Promise<void> => {      
-        const [startRow, startCol] = start;
-        const [endRow, endCol] = end;
-        const newBoard = board.map(row => [...row]);
-        newBoard[endRow][endCol] = newBoard[startRow][startCol];
-        newBoard[startRow][startCol] = '--';
-        setBoard(newBoard);
-    };
-    
+const PlayGame: React.FC = () => {    
     const { id } = useParams<{ id: string }>();
-    const initialBoardSetup = [
-        ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"],
-        ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["", "", "", "", "", "", "", ""],
-        ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-        ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"]
-    ];
-    const [board, setBoard] = React.useState<string[][]>(initialBoardSetup);
 
     return (
         <div className="">
             <h1 className="text-xl font-semibold mb-6">Play Game!</h1>
-            <ChessBoard isWhite={true} board={board} onMakeMove={onMakeMove} onMoveValidCheck={onMoveValidCheck} />
+            <h1 className="text-xl font-mono">{id}</h1>
         </div>
     );
 }
