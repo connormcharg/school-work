21b32eb5f7761d495c630794d04d3f6f7571525a
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2024-10-08 21:18:28 +0100

    tried to do board and failed

--- NEA-Final/RooksRealm/frontend/src/components/ChessBoard.tsx
+++ NEA-Final/RooksRealm/frontend/src/components/ChessBoard.tsx
@@ -1,1 +1,24 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import "./ChessBoard.css";
+
+interface ChessBoardProps {
+    isWhite: boolean;
+    board: string[][];
+    onMoveValidCheck: (start: number[], end: number[]) => Promise<boolean>;
+    onMakeMove: (start: number[], end: number[]) => Promise<void>;
+}
+
+const ChessBoard: React.FC<ChessBoardProps> = ({ isWhite, board, onMoveValidCheck, onMakeMove }) => {
+    const [selectedPiece, setSelectedPiece] = useState<HTMLElement | null>(null);
+    const [selectedPieceInitialRowCol, setSelectedPieceInitialRowCol] = useState<number[] | null>(null);
+    const [state, setState] = useState<number>(1);
+    const gridRef = useRef<HTMLDivElement>(null);
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ChessBoard;
